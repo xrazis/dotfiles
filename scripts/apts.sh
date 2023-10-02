8525d22f28b71f6f141accda30e36f6225ed8634
Haris Razis <haris@razis.com>
2023-10-02 15:52:22 +0300

    Clean up scripts

--- scripts/apts.sh
+++ scripts/apts.sh
@@ -9,7 +9,7 @@ apt install -y speedtest-cli
 echo "Installing restic..."
 apt install -y restic
 
-echo "Installing cURL"
+echo "Installing curl..."
 apt install -y curl
 
 echo "Installing httpie"
@@ -19,14 +19,8 @@ echo "Installing termius..."
 curl -OL https://www.termius.com/download/linux/Termius.deb
 dpkg -i Termius.deb
 
-echo "Installing vs code..."
+echo "Installing vscode..."
 apt install -y code
-
-echo "Installing firacode..."
-apt install -y fonts-firacode
-
-echo "Installing tilix..."
-apt install -y tilix  
 
 echo "Installing zsh..."
 apt install -y zsh
@@ -39,18 +33,9 @@ add-apt-repository -y \
   stable"
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+echo "Visit https://www.docker.com/ for latest version of docker-compose"
 
 echo "Installing system utilities..."
 apt install -y neofetch htop
 apt install -y cmake libevdev-dev libudev-dev libconfig++-dev # Depedencies for logiops
 apt install -y apt-transport-https ca-certificates gnupg-agent software-properties-common # Depedencies for docker
-
-echo "Installing signal..."
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg |  tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-  tee -a /etc/apt/sources.list.d/signal-xenial.list
-apt update && apt install signal-desktop
